4d410b575d4c4320db02d677a037e69f44653330
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-04-19 10:58:49 -0300

    Create script.sql

---
+++ Projeto ToDo/Script_DB/script.sql
@@ -0,0 +1,18 @@
+-- TABELA DE STATUS:
+CREATE TABLE tb_status (
+	id int PRIMARY KEY AUTO_INCREMENT,
+    status VARCHAR(15) NOT NULL
+);
+
+-- INSERÇÃO DOS DOIS STATUS QUE TEMOS NA APLICAÇÃO:
+INSERT INTO tb_status(status) VALUES('pendente');
+INSERT INTO tb_status(status) VALUES('realizado');
+
+-- TABELA DE TAREFAS:
+CREATE TABLE tb_tarefas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tarefa TEXT NOT NULL,
+    id_status INT NOT NULL DEFAULT 1,
+    data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ 	FOREIGN KEY(id_status) REFERENCES tb_status(id)
+);
