c904ca873c45059514ce8a4f19f434b5ad37e426
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-04-19 09:41:03 -0300

    Finalização do projeto

--- Projeto IMC/projeto_imc - código fonte/resultado_imc.php
+++ Projeto IMC/projeto_imc - código fonte/resultado_imc.php
@@ -23,7 +23,27 @@
     //desafio do tio: Criar um método de classificação, 
     //baseado neste link: https://arquivos.sbn.org.br/equacoes/eq5.htm 
     function classificaIMC($imc) {
-        return "calcular a classificação...";
+        $classificacao = "";
+
+        if($imc < 16) {
+            $classificacao = "Magreza grau III";
+        } elseif($imc >= 16 && $imc <= 16.9) {
+            $classificacao = "Magreza grau II";
+        } elseif($imc > 16.9 && $imc <= 18.4) {
+            $classificacao = "Magreza grau I";
+        } elseif($imc > 18.4 && $imc <= 24.9) {
+            $classificacao = "Adequado";
+        } elseif($imc > 24.9 && $imc <= 29.9) {
+            $classificacao = "Pré-obeso";
+        } elseif($imc > 29.9 && $imc <= 34.9) {
+            $classificacao = "Obesidade grau I";
+        } elseif($imc > 34.9 && $imc <= 39.9) {
+            $classificacao = "Obesidade grau II";
+        } else {
+            $classificacao = "Obesidade grau III";
+        }
+
+        return $classificacao;
     }
 
     ?>
@@ -39,7 +59,9 @@
         <p><b>IMC: </b><?= number_format($valor_imc, 2, ',') ?></p>
         <!-- Alternativa:
         <p><b>IMC: </b><?php printf('%.2f', $valor_imc); ?></p> -->
-        <p><b>Classificação: </b> <?= classificaIMC($valor_imc)?> </p>
+        <p><b>Classificação: </b> <?= classificaIMC($valor_imc)?></p>
+        <br>
+        <a href="./">Calcular novamente</a>
     </div>
 
 </body>
