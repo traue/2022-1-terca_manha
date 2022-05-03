4ef00260fdd6b38712e2dc939540cc2353f3d937
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-05-03 11:28:23 -0300

    Continuação do projeto

--- Projeto ToDo/codigo_fonte/todo (dentro da pasta)/nova_tarefa.php
+++ Projeto ToDo/codigo_fonte/todo (dentro da pasta)/nova_tarefa.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ToDo</title>
+    <title>ToDo - Nova Tarefa</title>
     <link href="css/estilo.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
 </head>
 
 <body>
@@ -26,11 +27,30 @@
         <div class="row">
             <div class="col-md-3 menu">
                 <ul>
-                    <li class="list-group-item active"><a href="#">Tarefas Pendentes</a></li>
-                    <li class="list-group-item"><a href="nova_tarefa.php">Nova Tarefa</a></li>
+                    <li class="list-group-item"><a href="index.php">Tarefas Pendentes</a></li>
+                    <li class="list-group-item active"><a href="nova_tarefa.php">Nova Tarefa</a></li>
                     <li class="list-group-item"><a href="todas_tarefas.php">Todas Tarefas</a></li>
                 </ul>
             </div>
+
+            <div class="col-md-9">
+                <div class="container pagina">
+                    <div class="row">
+                        <div class="col">
+                            <h4>Nova Tarefa:</h4>
+                            <hr/>
+                            <form>
+                                <div class="form-group">
+                                    <label>Tarefa:</label>
+                                    <input class="form-control" type="text" name="tarefa" placeholder="Exemplo: Comprar pão" required>
+                                </div>
+                                <button class="btn btn-outline-success">Adicionar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
